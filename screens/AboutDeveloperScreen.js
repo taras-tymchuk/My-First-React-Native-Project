ac43d3e6b7afd78040e53d5fe74b0ec200e013e6
Taras Tymchuk <tymakruk@gmail.com>
2019-01-13 14:37:26 +0200

    Added content to 'about developer' page

--- screens/AboutDeveloperScreen.js
+++ screens/AboutDeveloperScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Image, Text } from 'react-native';
-import { WebBrowser } from 'expo';
+import { WebBrowser, AuthSession } from 'expo';
 import { ScrollView } from 'react-native-gesture-handler';
 
 export default class AboutDeveloperScreen extends React.Component {
@@ -10,39 +10,65 @@ export default class AboutDeveloperScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView>
+      <View style={styles.container}>
         <View style={styles.imageContainer}>
-          <Image source={require('../assets/images/my-photo.png')}></Image>
+          <Image source={require('../assets/images/my-photo.png')} resizeMode={'contain'} style={styles.imageStyle}></Image>
         </View>
         <View style={styles.infoContainer}>
-          <Text>
-            lorem20
+          <Text style={styles.textBox}>
+            Hi again!
+            My name is Taras and I'm studying Web development at Softserve IT Academy. I'm 20 and I'm full of inspiration.
+            But let's speak not about programming. I have a few hobbies also. I'm keen on reading and football.
+            Missing match of my
+            <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}> favourite team </Text>
+            is impossible thing. That's it, shortly about myself :)
           </Text>
         </View>
-      </ScrollView>
+
+      </View>
     );
   }
 
   _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
+    WebBrowser.openBrowserAsync('https://www.fcbarcelona.com/en/');
   };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'aqua',
+    backgroundColor: 'blanchedalmond',
   },
   imageContainer: {
-
+    flex: 3,
+    marginTop: 40,
+    marginBottom: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    borderStyle: 'dashed',
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: 'black',
+    padding: 5,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  imageStyle: {
+    width: '100%'
   },
   infoContainer: {
-
-  }
+    flex: 2,
+    margin: 20,
+    marginTop: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textBox: {
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  helpLinkText: {
+    fontSize: 16,
+    color: '#2e78b7',
+  },
 });
